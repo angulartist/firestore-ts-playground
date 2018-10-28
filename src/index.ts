b79bc1aefc2ee6748df3d00914ccdcf2992cbb48
TechLead <techlead@MacBook-Pro-de-TechLead.local>
2018-10-28 11:17:18 +0100

    code refactoring

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import { Message } from "./models/message.model";
 class Chat {
   app: firebase.app.App;
   db: firebase.firestore.Firestore;
+  msgRef: firebase.firestore.Query;
 
   // DATA
   dataArray: any[] = [];
@@ -20,6 +21,8 @@ class Chat {
     this.db = this.app.firestore();
     this.db.settings({ timestampsInSnapshots: true });
 
+    this.msgRef = this.db.collection("slack").orderBy("postedAt", "asc");
+
     // INIT UI
     this.msgBox = document.querySelector("#message");
     this.button = document.querySelector("#send");
@@ -27,92 +30,34 @@ class Chat {
   }
 
   onLoad(): void {
-    const msgRef = this.db.collection("slack").orderBy("postedAt", "asc");
-
     // Get the data inside the firestore collection at time T.
     const retrieveData$ = new Promise((resolve, reject) => {
-      msgRef
+      this.msgRef
         .get()
         .then(snapshot => {
-          snapshot.docs.map(doc => {
-            const data = doc.data();
-            const { id } = doc;
-            const document = { id, ...data };
-            this.dataArray = [document, ...this.dataArray];
-          });
+          snapshot.docs.map(doc => this.updateData("init", doc));
           resolve();
         })
-        .catch(error => {
-          reject(error);
-        });
+        .catch(error => reject(error));
     });
 
     // Bind a listener and do sumffin whenever thing happens.
     retrieveData$
       .then(() => {
         this.render();
-        msgRef.onSnapshot(changes => {
-          changes.docChanges().map(changes => {
-            const changesEvents = {
-              added: () => {
-                if (!this.dataArray.some(e => e.id === changes.doc.id)) {
-                  const data = changes.doc.data();
-                  const { id } = changes.doc;
-                  const fireDoc = { id, ...data };
-                  this.dataArray = [fireDoc, ...this.dataArray];
-                  // update the UI
-                  return this.updateAddDoc(fireDoc);
-                }
-              },
-              removed: () => {
-                if (this.dataArray.some(e => e.id === changes.doc.id)) {
-                  const data = changes.doc.data();
-                  const { id } = changes.doc;
-                  const fireDoc = { id, ...data };
-                  this.dataArray = this.dataArray.filter(
-                    doc => doc.id !== fireDoc.id
-                  );
-                  // update the UI
-                  return this.updateRemoveDoc(fireDoc);
-                }
-              }
-            };
-
-            return changesEvents[changes.type]();
+        this.msgRef.onSnapshot(snapshot => {
+          snapshot.docChanges().map(changes => {
+            return {
+              added: () => this.updateData("add", changes.doc),
+              removed: () => this.updateData("remove", changes.doc)
+            }[changes.type]();
           });
         });
       })
-      .catch(error => {
-        console.log("Error happn.", "=>", error);
-      });
+      .catch(error => console.log("Error happn.", "=>", error));
   }
 
-  updateAddDoc(doc: Message): DocumentFragment {
-    const docFragment = document.createDocumentFragment();
-    // Message Wrapper
-    const messageWrapper = document.createElement("div");
-    messageWrapper.id = `${doc.id}`;
-    // Message Div
-    const messageDiv = document.createElement("div");
-    messageDiv.textContent = doc.msg;
-    messageDiv.addEventListener("click", () => {
-      this.removeMsg(doc);
-    });
-    // Append
-    messageWrapper.appendChild(messageDiv);
-    messageWrapper.querySelector("div").className = "show";
-    docFragment.appendChild(messageWrapper);
-
-    return this.msgBox.appendChild(docFragment);
-  }
-
-  updateRemoveDoc(doc: Message): void {
-    const docToRemove = document.getElementById(doc.id);
-    docToRemove.querySelector("div").className = "remove";
-    setTimeout(() => {
-      docToRemove.remove();
-    }, 300);
-  }
+  // Initial UI Render
 
   render(): DocumentFragment {
     const docFragment = document.createDocumentFragment();
@@ -136,6 +81,73 @@ class Chat {
     return this.msgBox.appendChild(docFragment);
   }
 
+  // DOM Manipulation
+
+  updateRender(type: string, doc: Message) {
+    return {
+      add: (): DocumentFragment => {
+        const docFragment = document.createDocumentFragment();
+        // Message Wrapper
+        const messageWrapper = document.createElement("div");
+        messageWrapper.id = `${doc.id}`;
+        // Message Div
+        const messageDiv = document.createElement("div");
+        messageDiv.textContent = doc.msg;
+        messageDiv.addEventListener("click", () => {
+          this.removeMsg(doc);
+        });
+        // Append
+        messageWrapper.appendChild(messageDiv);
+        messageWrapper.querySelector("div").className = "show";
+        docFragment.appendChild(messageWrapper);
+
+        return this.msgBox.appendChild(docFragment);
+      },
+      remove: (): void => {
+        const docToRemove = document.getElementById(doc.id);
+        docToRemove.querySelector("div").className = "remove";
+        setTimeout(() => {
+          docToRemove.remove();
+        }, 300);
+      }
+    }[type]();
+  }
+
+  // DATA Manipulation
+
+  updateData(type: string, doc: firebase.firestore.QueryDocumentSnapshot) {
+    return {
+      init: () => {
+        const data = doc.data();
+        const { id } = doc;
+        const document = { id, ...data };
+        return (this.dataArray = [document, ...this.dataArray]);
+      },
+      add: () => {
+        if (!this.dataArray.some(el => el.id === doc.id)) {
+          const data = doc.data();
+          const { id } = doc;
+          const fireDoc = { id, ...data };
+          this.dataArray = [fireDoc, ...this.dataArray];
+          // update the UI
+          return this.updateRender("add", fireDoc);
+        }
+      },
+      remove: () => {
+        if (this.dataArray.some(el => el.id === doc.id)) {
+          const data = doc.data();
+          const { id } = doc;
+          const fireDoc = { id, ...data };
+          this.dataArray = this.dataArray.filter(el => el.id !== fireDoc.id);
+          // update the UI
+          return this.updateRender("remove", fireDoc);
+        }
+      }
+    }[type]();
+  }
+
+  // Firestore CRUD
+
   addMsg(): Promise<firebase.firestore.DocumentReference> {
     let randomName = Faker.random.words();
     return this.db
@@ -143,7 +155,7 @@ class Chat {
       .add({ msg: randomName, postedAt: new Date() });
   }
 
-  editMsg({ id: documentId }: Message) {
+  editMsg({ id: documentId }: Message): Promise<void> {
     let randomName = Faker.hacker.phrase;
     return this.db.doc(`slack/${documentId}`).update({ msg: randomName });
   }
@@ -151,6 +163,8 @@ class Chat {
   removeMsg({ id: documentId }: Message): Promise<void> {
     return this.db.doc(`slack/${documentId}`).delete();
   }
+
+  // GETTERS
 
   get timestamp(): firebase.firestore.FieldValue {
     return firebase.firestore.FieldValue.serverTimestamp();
